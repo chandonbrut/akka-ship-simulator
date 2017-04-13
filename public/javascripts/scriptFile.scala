cd52650cb4fd4d9789b1b5842144d63d81e71644
Jonas <jonas.nolife@gmail.com>
2017-04-13 16:28:06 -0300

    adding cesium

---
+++ public/javascripts/scriptFile.scala
@@ -0,0 +1,5 @@
+/home/jferreira/lrit3d/Cesium
+/home/jferreira/lrit3d/exports.js
+/home/jferreira/lrit3d/olcesium.js
+/home/jferreira/lrit3d/olcesium.js.map
+/home/jferreira/lrit3d/olcesium-debug.js
